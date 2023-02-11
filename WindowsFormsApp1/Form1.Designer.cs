5f41e36ec79f682365eb2eddc505287e97b9ebbd
Bessygaby <124412106+Bessygaby@users.noreply.github.com>
2023-02-11 10:53:35 -0600

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtGrade1 = new System.Windows.Forms.TextBox();
+            this.txtGrade2 = new System.Windows.Forms.TextBox();
+            this.txtGrade3 = new System.Windows.Forms.TextBox();
+            this.txtGrade4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(199, 205);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(161, 49);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calcular";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtGrade1
+            // 
+            this.txtGrade1.Location = new System.Drawing.Point(199, 31);
+            this.txtGrade1.Name = "txtGrade1";
+            this.txtGrade1.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade1.TabIndex = 1;
+            // 
+            // txtGrade2
+            // 
+            this.txtGrade2.Location = new System.Drawing.Point(199, 76);
+            this.txtGrade2.Name = "txtGrade2";
+            this.txtGrade2.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade2.TabIndex = 2;
+            // 
+            // txtGrade3
+            // 
+            this.txtGrade3.Location = new System.Drawing.Point(199, 119);
+            this.txtGrade3.Name = "txtGrade3";
+            this.txtGrade3.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade3.TabIndex = 3;
+            // 
+            // txtGrade4
+            // 
+            this.txtGrade4.Location = new System.Drawing.Point(199, 160);
+            this.txtGrade4.Name = "txtGrade4";
+            this.txtGrade4.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NOTA # 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "NOTA # 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "NOTA # 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "NOTA # 4";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(427, 84);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(430, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Promedio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 331);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGrade4);
+            this.Controls.Add(this.txtGrade3);
+            this.Controls.Add(this.txtGrade2);
+            this.Controls.Add(this.txtGrade1);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtGrade1;
+        private System.Windows.Forms.TextBox txtGrade2;
+        private System.Windows.Forms.TextBox txtGrade3;
+        private System.Windows.Forms.TextBox txtGrade4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double grade1 = Convert.ToDouble(txtGrade1.Text);
+            double grade2 = Convert.ToDouble(txtGrade2.Text);
+            double grade3 = Convert.ToDouble(txtGrade3.Text);
+            double grade4 = Convert.ToDouble(txtGrade4.Text);
+
+            double average = await CalculateAverageAsync(grade1, grade2, grade3, grade4);
+            lblResult.Text = "Promedio Final: " + average.ToString("F");
+        }
+
+
+        private async System.Threading.Tasks.Task<double> CalculateAverageAsync(double grade1, double grade2, double grade3, double grade4)
+        {
+            return await System.Threading.Tasks.Task.Run(() => (grade1 + grade2 + grade3 + grade4) / 4);
+        }
+    }
+}
